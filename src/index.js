678a6fa260900dfe0c1e9f6c3e9773408785c807
Evgeniy <nikolaev_zheka-zik@bk.ru>
2022-05-12 17:43:14 +0300

    test1

--- src/index.js
+++ src/index.js
@@ -1,10 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import App from './components/app/App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const rootElement = document.getElementById('root')
+ReactDOM.render(App({prop: 5, prop2: 17}), rootElement);
